b4397bfdf65c7b64ff11074b32b8d9575c5dbcde
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-10-11 17:48:37 -0300

    Testando variáveis com int e double e analisando resultados

---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,27 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main (String[] args) {
+		
+		//Variável double é usada em valores com decimais.
+		double salario;
+		salario = 1500.50;
+		System.out.println("Meu salario é: " + salario);
+		
+		double divisao;
+		divisao = 3.14 / 2;
+		System.out.println("A divisão é: " + divisao);
+		
+		//Dividir números inteiros gera como resultado um inteiro
+		int outraDivisao;
+		outraDivisao = 5 / 2;
+		System.out.println("A divisão é: " + outraDivisao);
+		
+		//Dividir números doubles
+		double novaTentativa;
+		novaTentativa = 5.0 / 2; 
+		System.out.println("A divisão é: " + novaTentativa);
+		
+	}
+
+}
