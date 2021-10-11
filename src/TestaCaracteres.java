9f04a28adf008a76cea33782feeda3e5e7f11116
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-10-11 18:42:53 -0300

    Testando e vendo a diferença entre char e String

---
+++ src/TestaCaracteres.java
@@ -0,0 +1,21 @@
+
+public class TestaCaracteres {
+	
+	public static void main (String[] args) {
+		//Char somente permite guardar uma letra só
+		//char é declarado com aspas simples
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char)(valor + 1);
+		System.out.println(valor);
+		
+		//Uso da String
+		//String é declarado com aspas duplas
+		String palavra = "Alura, cursos online de tecnologia";
+		System.out.println(palavra);	
+	}
+}
